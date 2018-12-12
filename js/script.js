3cc9058dbd739819ae71c84c88bfa72d7c5e6b6c
Anton Romankov <romant094@gmail.com>
2018-12-13 01:34:46 +0300

    Modal popup show/hide

--- js/script.js
+++ js/script.js
@@ -2,17 +2,29 @@ let d = document,
     popupEngineerBtn = d.querySelector('.popup_engineer_btn'),
     popupEngineer = d.querySelector('.popup_engineer'),
     popupEngineerContent = d.querySelector('.popup_engineer .popup_content'),
+    phoneLink = d.querySelectorAll('.phone_link'),
+    popup = d.querySelector('.popup'),
+    popupContent = d.querySelector('.popup .popup_content'),
     popupClose = d.querySelectorAll('.popup_close');
 
 // Modals
 popupEngineerBtn.addEventListener('click', () => {
     showHideModal(popupEngineer);
 });
+phoneLink.forEach((link) => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        showHideModal(popup);
+    });
+});
 
 d.addEventListener('click', (event) => {
     let t = event.target;
     if ((t == popupEngineer || t == popupClose[1]) && t != popupEngineerContent) {
         showHideModal(popupEngineer);
+    }
+    if ((t == popup || t == popupClose[0]) && t != popupContent) {
+        showHideModal(popup);
     }
 });
 
